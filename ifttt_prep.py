5cf0f6dd476fe2a48a02ea94f9ed14ebff69a0c9
ekeneattoh <ekeneattoh@gmail.com>
2022-05-24 09:00:49 +0200

    optimised algorithm

--- ifttt_prep.py
+++ ifttt_prep.py
@@ -21,27 +21,22 @@ def populate_ifttt_datasets(filename: str) -> list:
 
 def process_ifttt_rules(doc1: str, word_list: list, raw_results_subfolder_name: str,
                         filtered_results_subfolder_name: str):
-    result = compute_spacy_word_similarity(doc1=doc1, word_list=word_list)
-
     main_folder = "processed_data/"
 
     raw_data_filename = main_folder + raw_results_subfolder_name + doc1 + "_ifttt_result.json"
 
     filtered_data_filename = main_folder + filtered_results_subfolder_name + doc1 + "_ifttt_result.json"
 
-    # write the result to a json file
-    if path.exists(raw_data_filename):
-        print(raw_data_filename + " has already  been created!")
+    if path.exists(raw_data_filename) and path.exists(filtered_data_filename):
+        print(doc1 + " files have already  been created!")
     else:
-        # write the raw processed_data to file
+        result = compute_spacy_word_similarity(doc1=doc1, word_list=word_list)
+
         write_to_json_file(filename=raw_data_filename, data=result)
-        print(raw_data_filename + " was just created!")
-    if path.exists(filtered_data_filename):
-        print(filtered_data_filename + " has already  been created!")
-    else:
-        # write the filtered processed data to file, i.e remove data with similarity below a threshold
+
         write_to_json_file(filename=filtered_data_filename, data=filter_spacy_result(dataset=result, threshold=55))
-        print(filtered_data_filename + " was just created!")
+
+        print(doc1 + " files were just created!")
 
 
 def remove_special_characters(dataset: dict, dataset_key: str):
